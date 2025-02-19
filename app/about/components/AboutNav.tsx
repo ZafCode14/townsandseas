c599a9de6b43564aeca4a7915fe53cbbd0fc477f
ZafCode14 <zafarwp14@gmail.com>
2025-02-19 18:49:01 +0200

    typos

--- app/about/components/AboutNav.tsx
+++ app/about/components/AboutNav.tsx
@@ -11,7 +11,7 @@ export default function AboutNav() {
     <div className="uppercase text-[14px] md:text-[20px] lg:text-[24px] relative lg:top-[40px] pl-5 md:pl-0">
       <p className={`mb-2 hover:underline cursor-pointer ${active === "about" && "underline"}`} onClick={() => dispatch(toggleAbout("about"))}>about us</p>
       <p className={`mb-2 hover:underline cursor-pointer ${active === "mission" && "underline"}`} onClick={() => dispatch(toggleAbout("mission"))}>OUR ETHOS</p>
-      <p className={`mb-2 hover:underline cursor-pointer ${active === "goals" && "underline"}`} onClick={() => dispatch(toggleAbout("goals"))}>GOALS & OBJECTIVE</p>
+      <p className={`mb-2 hover:underline cursor-pointer ${active === "goals" && "underline"}`} onClick={() => dispatch(toggleAbout("goals"))}>GOALS & OBJECTIVES</p>
       <p className={`mb-2 hover:underline cursor-pointer ${active === "team" && "underline"}`} onClick={() => dispatch(toggleAbout("team"))}>PEOPLE BEHIND THE CRAFT</p>
       {/**
       <p className={`hover:underline cursor-pointer ${active === "values" && "underline"}`} onClick={() => dispatch(toggleAbout("values"))}>core values</p>
