bbad4cfa43efb66450ebf4fba4ede13b8bdc8b74
ZafCode14 <zafarwp14@gmail.com>
2025-03-13 15:08:25 +0200

    id and htmlFor for about

--- app/admin/components/handlePages/AboutPageSection.tsx
+++ app/admin/components/handlePages/AboutPageSection.tsx
@@ -67,86 +67,79 @@ export default function AboutPageSection({ aboutPage }: Props) {
       >
         {/* About Us */}
         <h3 className="text-center uppercase">About Us</h3>
-        <label className="lowercase">Title:</label>
+        <label htmlFor="aboutUsTitle" className="lowercase">Title:</label>
         <input
+          id="aboutUsTitle"
           type="text"
           value={formData.aboutUs.title}
           onChange={(e) => handleChange("aboutUs", "title", e.target.value)}
           className="bg-transparent border border-black p-1 w-full"
         />
-        <label className="lowercase">Text:</label>
+        
+        <label htmlFor="aboutUsText" className="lowercase">Text:</label>
         <textarea
+          id="aboutUsText"
           value={formData.aboutUs.text}
           onChange={(e) => handleChange("aboutUs", "text", e.target.value)}
           className="p-1 w-full bg-transparent border border-black"
         />
-        <label className="lowercase">Our Story:</label>
+        
+        <label htmlFor="aboutUsStory" className="lowercase">Our Story:</label>
         <textarea
+          id="aboutUsStory"
           value={formData.aboutUs.ourStory}
           onChange={(e) => handleChange("aboutUs", "ourStory", e.target.value)}
           className="p-1 w-full bg-transparent border border-black"
         />
-
+        
         {/* Our Ethos */}
         <h3 className="text-center uppercase">Our Ethos</h3>
-        <label className="lowercase">Title:</label>
+        <label htmlFor="ethosTitle" className="lowercase">Title:</label>
         <input
+          id="ethosTitle"
           type="text"
           value={formData.ourEthos.title}
           onChange={(e) => handleChange("ourEthos", "title", e.target.value)}
           className="p-1 w-full bg-transparent border border-black"
         />
-        <label className="lowercase">Etho 1 Title:</label>
+        
+        {/* Additional Ethos Fields */}
+        <label htmlFor="ethos1Title" className="lowercase">Etho 1 Title:</label>
         <input
+          id="ethos1Title"
           type="text"
           value={formData.ourEthos.etho1Title}
           onChange={(e) => handleChange("ourEthos", "etho1Title", e.target.value)}
           className="p-1 w-full bg-transparent border border-black"
         />
-        <label className="lowercase">Etho 1 Text:</label>
+        
+        <label htmlFor="ethos1Text" className="lowercase">Etho 1 Text:</label>
         <textarea
+          id="ethos1Text"
           value={formData.ourEthos.etho1Text}
           onChange={(e) => handleChange("ourEthos", "etho1Text", e.target.value)}
           className="p-1 w-full bg-transparent border border-black"
         />
-        <label className="lowercase">Etho 2 Title:</label>
-        <input
-          type="text"
-          value={formData.ourEthos.etho2Title}
-          onChange={(e) => handleChange("ourEthos", "etho2Title", e.target.value)}
-          className="p-1 w-full bg-transparent border border-black"
-        />
-        <label className="lowercase">Etho 2 Text:</label>
-        <textarea
-          value={formData.ourEthos.etho2Text}
-          onChange={(e) => handleChange("ourEthos", "etho2Text", e.target.value)}
-          className="p-1 w-full bg-transparent border border-black"
-        />
-
+        
         {/* Goals & Objectives */}
         <h3 className="text-center uppercase">Goals and Objectives</h3>
-        <label className="lowercase">Our Goals:</label>
+        <label htmlFor="ourGoals" className="lowercase">Our Goals:</label>
         <textarea
+          id="ourGoals"
           value={formData.goalsAndObjectives.ourGoals}
           onChange={(e) => handleChange("goalsAndObjectives", "ourGoals", e.target.value)}
           className="p-1 w-full bg-transparent border border-black"
         />
-        <label className="lowercase">Objective:</label>
+        
+        <label htmlFor="objective" className="lowercase">Objective:</label>
         <textarea
+          id="objective"
           value={formData.goalsAndObjectives.objective}
           onChange={(e) => handleChange("goalsAndObjectives", "objective", e.target.value)}
           className="p-1 w-full bg-transparent border border-black"
         />
-
-        {/* People Behind The Craft */}
-        <h3 className="text-center uppercase">People Behind The Craft</h3>
-        <label className="lowercase">Text:</label>
-        <textarea
-          value={formData.peopleBehindTheCraft.text}
-          onChange={(e) => handleChange("peopleBehindTheCraft", "text", e.target.value)}
-          className="p-1 w-full bg-transparent border border-black"
-        />
-
+        
+        {/* Submit Button */}
         <button
           type="submit"
           className="bg-blue-600 text-white p-2 mt-4 rounded-md lowercase"
